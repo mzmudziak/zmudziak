5a2e72ed2eabc8af25aa7c8617e2fd1ea36d76f9
Michal Zmudziak <Michal.Zmudziak@ibm.com>
2019-03-06 18:29:26 +0100

    first commit

---
+++ src/main/java/com/umcs/App.java
@@ -0,0 +1,13 @@
+package com.umcs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
